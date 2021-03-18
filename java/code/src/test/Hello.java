7b2c289d3137333bf5a1035d8efba600b32f6fbb
sece <hao1233626@qq.com>
2021-03-18 13:57:25 +0800

    use ide

---
+++ java/code/src/test/Hello.java
@@ -0,0 +1,12 @@
+package test;
+
+/**
+ * @author sece
+ * @version 1.0
+ * @since 2020-02-04
+ */
+public class Hello {
+    public static void main(String[] args){
+        System.out.println("Hello,world!");
+    }
+}
